cf61be8e49f807012213458bed90a253f818f947
Ryo Kuramoto <kuramoto@n2sm.net>
2018-10-19 15:09:28 +0900

    impl getAccessToken & getClient & processDriveItem

--- src/main/java/org/codelibs/fess/ds/office365/Office365DataStore.java
+++ src/main/java/org/codelibs/fess/ds/office365/Office365DataStore.java
@@ -15,15 +15,43 @@
  */
 package org.codelibs.fess.ds.office365;
 
-import java.util.Map;
-
+import com.microsoft.aad.adal4j.AuthenticationContext;
+import com.microsoft.aad.adal4j.AuthenticationResult;
+import com.microsoft.aad.adal4j.ClientCredential;
+import com.microsoft.graph.logger.DefaultLogger;
+import com.microsoft.graph.models.extensions.DriveItem;
+import com.microsoft.graph.models.extensions.IGraphServiceClient;
+import com.microsoft.graph.requests.extensions.GraphServiceClient;
+import org.codelibs.fess.crawler.exception.CrawlingAccessException;
 import org.codelibs.fess.ds.AbstractDataStore;
 import org.codelibs.fess.ds.callback.IndexUpdateCallback;
 import org.codelibs.fess.es.config.exentity.DataConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.net.MalformedURLException;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+
 public class Office365DataStore extends AbstractDataStore {
+
+    // parameters
+    private static final String TENANT_PARAM = "tenant";
+    private static final String CLIENT_ID_PARAM = "client_id";
+    private static final String CLIENT_SECRET_PARAM = "client_secret";
+
+    // scripts
+    private static final String FILES = "files";
+    private static final String FILES_NAME = "name";
+    private static final String FILES_DESCRIPTION = "description";
+    private static final String FILES_CONTENTS = "contents";
+    private static final String FILES_MIMETYPE = "mimetype";
+    private static final String FILES_CREATED = "created";
+    private static final String FILES_LAST_MODIFIED = "last_modified";
+    private static final String FILES_WEB_URL = "web_url";
 
     private static final Logger logger = LoggerFactory.getLogger(Office365DataStore.class);
 
@@ -34,6 +62,86 @@ public class Office365DataStore extends AbstractDataStore {
     @Override
     protected void storeData(final DataConfig dataConfig, final IndexUpdateCallback callback, final Map<String, String> paramMap,
             final Map<String, String> scriptMap, final Map<String, Object> defaultDataMap) {
+
+        final String tenant = paramMap.getOrDefault(TENANT_PARAM, "");
+        final String clientId = paramMap.getOrDefault(CLIENT_ID_PARAM, "");
+        final String clientSecret = paramMap.getOrDefault(CLIENT_SECRET_PARAM, "");
+
+        if (tenant.isEmpty() || clientId.isEmpty() || clientSecret.isEmpty()) {
+            logger.warn("parameter '" + //
+                    TENANT_PARAM + "', '" + //
+                    CLIENT_ID_PARAM + "', '" + //
+                    CLIENT_SECRET_PARAM + "' is required");
+            return;
+        }
+
+        final String accessToken;
+        try {
+            accessToken = getAccessToken(tenant, clientId, clientSecret);
+        } catch (final MalformedURLException | ExecutionException | InterruptedException e) {
+            logger.warn("failed to get access token.", e);
+            return;
+        }
+
+        final IGraphServiceClient client = getClient(accessToken);
+
+    }
+
+    protected void processDriveItem(final IndexUpdateCallback callback, final Map<String, String> paramMap,
+            final Map<String, String> scriptMap, final Map<String, Object> defaultDataMap, final DriveItem item) {
+        final Map<String, Object> dataMap = new HashMap<>(defaultDataMap);
+        final Map<String, Object> resultMap = new LinkedHashMap<>(paramMap);
+        final Map<String, Object> filesMap = new HashMap<>();
+
+        filesMap.put(FILES_NAME, item.name);
+        filesMap.put(FILES_DESCRIPTION, item.description);
+        filesMap.put(FILES_CONTENTS, getDriveItemContents(item));
+        filesMap.put(FILES_MIMETYPE, item.file.mimeType);
+        filesMap.put(FILES_CREATED, item.createdDateTime.getTime());
+        filesMap.put(FILES_LAST_MODIFIED, item.lastModifiedDateTime.getTime());
+        filesMap.put(FILES_WEB_URL, item.webUrl);
+        resultMap.put(FILES, filesMap);
+
+        try {
+            for (final Map.Entry<String, String> entry : scriptMap.entrySet()) {
+                final Object convertValue = convertValue(entry.getValue(), resultMap);
+                if (convertValue != null) {
+                    dataMap.put(entry.getKey(), convertValue);
+                }
+            }
+            callback.store(paramMap, dataMap);
+        } catch (final CrawlingAccessException e) {
+            logger.warn("Crawling Access Exception at : " + dataMap, e);
+        }
+    }
+
+    protected static String getDriveItemContents(final DriveItem item) {
+        return "";
+    }
+
+    private static String getAccessToken(final String tenant, final String clientId, final String clientSecret)
+            throws MalformedURLException, ExecutionException, InterruptedException {
+        final AuthenticationContext context =
+                new AuthenticationContext("https://login.microsoftonline.com/" + tenant + "/", false, Executors.newFixedThreadPool(1));
+        final AuthenticationResult result =
+                context.acquireToken("https://graph.microsoft.com", new ClientCredential(clientId, clientSecret), null).get();
+        return result.getAccessToken();
+    }
+
+    private static IGraphServiceClient getClient(final String accessToken) {
+        return GraphServiceClient.builder() //
+                .authenticationProvider(request -> request.addHeader("Authorization", "Bearer " + accessToken)) //
+                .logger(new DefaultLogger() {
+                    @Override
+                    public void logDebug(String message) {
+                        logger.debug(message);
+                    }
+
+                    @Override
+                    public void logError(String message, Throwable throwable) {
+                        logger.error(message, throwable);
+                    }
+                }).buildClient();
     }
 
 }
--- src/test/java/org/codelibs/fess/ds/office365/Office365DataStoreTest.java
+++ src/test/java/org/codelibs/fess/ds/office365/Office365DataStoreTest.java
@@ -15,13 +15,22 @@
  */
 package org.codelibs.fess.ds.office365;
 
-import org.codelibs.fess.ds.office365.Office365DataStore;
+import com.microsoft.graph.models.extensions.DriveItem;
+import com.microsoft.graph.models.extensions.File;
+import org.codelibs.fess.ds.callback.IndexUpdateCallbackImpl;
+import org.codelibs.fess.es.config.exentity.DataConfig;
+import org.codelibs.fess.mylasta.direction.FessConfig;
 import org.codelibs.fess.util.ComponentUtil;
 import org.dbflute.utflute.lastadi.ContainerTestCase;
 
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+
 public class Office365DataStoreTest extends ContainerTestCase {
 
-    public Office365DataStore dataStore;
+    private Office365DataStore dataStore;
 
     @Override
     protected String prepareConfigFile() {
@@ -45,9 +54,72 @@ public class Office365DataStoreTest extends ContainerTestCase {
         super.tearDown();
     }
 
-    public void test_xxx() {
-        // TODO
-        assertTrue(true);
+    public void testProduction() {
+        // doStoreDataTest();
+    }
+
+    public void doStoreDataTest() {
+        final DataConfig dataConfig = new DataConfig();
+        final Map<String, String> paramMap = new HashMap<>();
+        paramMap.put("tenant", "");
+        paramMap.put("client_id", "");
+        paramMap.put("client_secret", "");
+        final Map<String, String> scriptMap = new HashMap<>();
+        final Map<String, Object> defaultDataMap = new HashMap<>();
+
+        final FessConfig fessConfig = ComponentUtil.getFessConfig();
+        scriptMap.put(fessConfig.getIndexFieldTitle(), "files.name");
+        scriptMap.put(fessConfig.getIndexFieldContent(), "files.description + \"\\n\"+ files.contents");
+        scriptMap.put(fessConfig.getIndexFieldMimetype(), "files.mimetype");
+        scriptMap.put(fessConfig.getIndexFieldCreated(), "files.created");
+        scriptMap.put(fessConfig.getIndexFieldLastModified(), "files.last_modified");
+        scriptMap.put(fessConfig.getIndexFieldUrl(), "files.web_url");
+
+        dataStore.storeData(dataConfig, new IndexUpdateCallbackImpl() {
+            @Override
+            public void store(Map<String, String> paramMap, Map<String, Object> dataMap) {
+                System.out.println(dataMap);
+            }
+        }, paramMap, scriptMap, defaultDataMap);
+    }
+
+    public void testProcessDriveItem() throws Exception {
+        final Map<String, String> paramMap = new HashMap<>();
+        final Map<String, String> scriptMap = new HashMap<>();
+        scriptMap.put("name", "files.name");
+        scriptMap.put("description", "files.description");
+        scriptMap.put("mimetype", "files.mimetype");
+        scriptMap.put("created", "files.created");
+        scriptMap.put("last_modified", "files.last_modified");
+        scriptMap.put("web_url", "files.web_url");
+        final Map<String, Object> defaultDataMap = new HashMap<>();
+        final DriveItem item = new DriveItem();
+        item.name = "hoge";
+        item.description = "hogehoge";
+        item.file = new File();
+        item.file.mimeType = "fuga";
+        item.createdDateTime = Calendar.getInstance();
+        item.lastModifiedDateTime = Calendar.getInstance();
+        item.webUrl = "piyo";
+        final CountDownLatch latch = new CountDownLatch(1);
+        dataStore.processDriveItem(new IndexUpdateCallbackImpl() {
+            @Override
+            public void store(Map<String, String> paramMap, Map<String, Object> dataMap) {
+                assertEquals(item.name, dataMap.get("name"));
+                assertEquals(item.description, dataMap.get("description"));
+                assertEquals(item.file.mimeType, dataMap.get("mimetype"));
+                assertEquals(item.createdDateTime.getTime(), dataMap.get("created"));
+                assertEquals(item.lastModifiedDateTime.getTime(), dataMap.get("last_modified"));
+                assertEquals(item.webUrl, dataMap.get("web_url"));
+                latch.countDown();
+            }
+        }, paramMap, scriptMap, defaultDataMap, item);
+        latch.await();
+    }
+
+    public void testGetDriveItemContents() {
+        final DriveItem item = new DriveItem();
+        assertEquals("", Office365DataStore.getDriveItemContents(item));
     }
 
 }
